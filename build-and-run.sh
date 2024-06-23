242735e400549b3322de5e1255b8b0dcf4992058
homorunner <homoctrl@gmail.com>
2024-06-23 13:58:13 +0000

    Update -std=c++17

--- build-and-run.sh
+++ build-and-run.sh
@@ -4,7 +4,7 @@ set -e
 target=${1:-$(find . -type f -name "*.cpp" -exec stat --format '%Y :%y %n' "{}" \; | sort -nr | cut -d' ' -f5- | head -n 1)}
 
 echo "Building $target"
-g++ -o output -O3 $target
+g++ -o output -O3 -std=c++17 $target
 echo "Running:"
 ./output > output.txt
 echo "Output:"
