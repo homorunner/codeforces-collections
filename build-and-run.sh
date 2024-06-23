b332b80177d5e023d8c03112f049f9c38c4f75e7
homorunner <homoctrl@gmail.com>
2024-06-23 11:59:08 +0000

    Initialize project with build script

---
+++ build-and-run.sh
@@ -0,0 +1,6 @@
+# if first argument is not provided, build and runs the latest edited file.
+target=${1:-$(find . -type f -name "*.cpp" -exec stat --format '%Y :%y %n' "{}" \; | sort -nr | cut -d' ' -f5- | head -n 1)}
+
+echo "Building $target"
+g++ -o output -O3 $target && echo "Running:" && ./output
+rm output
